93055ca1c127644966841a30586ae0a371191d5f
Jose-76825708 <76825708@continental.edu.pe>
2026-04-29 01:54:14 -0500

    refactorizacion de la estructura de la clase doctor

--- src/main/java/com/example/demo/Doctor/Doctor.java
+++ src/main/java/com/example/demo/Doctor/Doctor.java
@@ -1,7 +1,10 @@
 package com.example.demo.Doctor;
 
+import com.example.demo.Cita.Cita;
 import org.springframework.format.annotation.DateTimeFormat;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Doctor {
 
@@ -14,14 +17,10 @@ public class Doctor {
     // Esta es la etiqueta que soluciona el Error 400
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate fechaNacimiento;
-
-    // Constructor vacío (Obligatorio cuando usas @ModelAttribute)
-    public Doctor() {
-    }
+    private List<Cita> citas = new ArrayList<>();
 
     // Constructor con parámetros
-    public Doctor(int id, String nombre, String dni, String especialidad, String telefono, LocalDate fechaNacimiento) {
-        this.id = id;
+    public Doctor(String nombre, String dni, String especialidad, String telefono, LocalDate fechaNacimiento) {
         this.nombre = nombre;
         this.dni = dni;
         this.especialidad = especialidad;
@@ -34,47 +33,56 @@ public class Doctor {
         return id;
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public String getNombre() {
         return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getDni() {
         return dni;
     }
 
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
     public String getEspecialidad() {
         return especialidad;
-    }
-
-    public void setEspecialidad(String especialidad) {
-        this.especialidad = especialidad;
     }
 
     public String getTelefono() {
         return telefono;
     }
 
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
     public LocalDate getFechaNacimiento() {
         return fechaNacimiento;
     }
 
+    public List<Cita> getCitas () {
+        return citas;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
     public void setFechaNacimiento(LocalDate fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public void setCitas(List<Cita> citas) {
+        this.citas = citas;
     }
 }
