929c283c3fe22dfa69558e6d734991ded58f7c04
Jose-76825708 <76825708@continental.edu.pe>
2026-05-01 09:27:37 -0500

    corrigiendo estructura del objeto CitaServiceImpl

--- src/main/java/com/example/demo/Cita/CitaServiceImpl.java
+++ src/main/java/com/example/demo/Cita/CitaServiceImpl.java
@@ -1,15 +1,33 @@
 package com.example.demo.Cita;
 
+import com.example.demo.Doctor.Doctor;
+import com.example.demo.Doctor.DoctorService;
+import com.example.demo.Paciente.Paciente;
+import com.example.demo.Paciente.PacienteService;
+import com.example.demo.Servicio.Servicio;
+import com.example.demo.Servicio.ServicioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Service
 public class CitaServiceImpl implements CitaService {
 
-    @Autowired
-    private CitaDAO citaDAO;
+    private final CitaDAO citaDAO;
+    private final PacienteService pacienteService;
+    private final DoctorService doctorService;
+    private final ServicioService servicioService;
+
+    public CitaServiceImpl(CitaDAO citaDAO, PacienteService pacienteService ,DoctorService doctorService ,ServicioService servicioService ){
+        this.citaDAO = citaDAO;
+        this.pacienteService = pacienteService;
+        this.doctorService = doctorService;
+        this.servicioService = servicioService;
+    }
+
 
     @Override
     public List<Cita> listar() {
@@ -17,8 +35,15 @@ public class CitaServiceImpl implements CitaService {
     }
 
     @Override
-    public void guardar(Cita cita) {
-        citaDAO.guardar(cita);
+    public void guardar(int pacienteId, int doctorId, int servicioId, LocalDate fecha, LocalTime hora, String estado) {
+
+        Paciente p = pacienteService.buscarPorId(pacienteId);
+        Doctor d = doctorService.buscarPorId(doctorId);
+        Servicio s = servicioService.buscarPorId(servicioId);
+
+        Cita c = new Cita(p,d,s,fecha, hora,estado);
+
+        citaDAO.guardar(c);
     }
 
     @Override
@@ -32,7 +57,15 @@ public class CitaServiceImpl implements CitaService {
     }
 
     @Override
-    public void actualizar(Cita cita) {
-        citaDAO.actualizar(cita);
+    public void actualizar(int id ,int pacienteId, int doctorId, int servicioId, LocalDate fecha, LocalTime hora, String estado) {
+
+        Paciente p = pacienteService.buscarPorId(pacienteId);
+        Doctor d = doctorService.buscarPorId(doctorId);
+        Servicio s = servicioService.buscarPorId(servicioId);
+
+        Cita c = new Cita(p,d,s,fecha, hora,estado);
+        c.setId(id);
+
+        citaDAO.actualizar(c);
     }
 }
